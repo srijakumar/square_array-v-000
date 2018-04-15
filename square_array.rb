d59b4eb81939782f2235f2c869d2e1d790aed34a
Srija S <srijakumar7@gmail.com>
2018-04-15 13:37:22 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,20 @@
 def square_array(array)
+<<<<<<< HEAD
   squared = []
   array.each do |element|
   squared << element*element
   end
   squared
+=======
+  array.each do |element|
+  return element*element
+  end
+end
+
+
+def square_array(array)
+  array.collect do|element|
+    return element*2
+  end
+>>>>>>> ac6ad35d1b1c2a40c549850898ac2100fe627ab3
 end
